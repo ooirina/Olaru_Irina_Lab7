d7c2ca6146f8d4c201c055fb11958c055c44da85
ooirina <200990762+ooirina@users.noreply.github.com>
2025-11-26 12:26:04 +0200

    Lab 8 -lista de cumparaturi pe windows, de adaugat

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,37 @@
-﻿namespace OlaruIrinaLab7
+﻿using System;
+using OlaruIrinaLab7.Data;
+using System.IO;
+
+
+namespace OlaruIrinaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+           
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
