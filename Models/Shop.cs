e27aa3a7659c4af129ea330ebf827b557792595c
ooirina <200990762+ooirina@users.noreply.github.com>
2025-12-03 18:46:12 +0200

    Lab 10-Geolocation

---
+++ Models/Shop.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteNetExtensions.Attributes;   
+
+namespace OlaruIrinaLab7.Models
+{
+    public class Shop
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string ShopName { get; set; }
+        public string Adress { get; set; }
+        public string ShopDetails
+        {
+            get
+            {
+                return ShopName + " "+Adress;} }
+        [OneToMany]
+ public List<ShopList> ShopLists { get; set; }
+    }
+}
---
+++ ShopPage.xaml.cs
@@ -0,0 +1,34 @@
+using OlaruIrinaLab7.Models;
+
+namespace OlaruIrinaLab7;
+
+public partial class ShopPage : ContentPage
+{
+    public ShopPage()
+    {
+     InitializeComponent();
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        await App.Database.SaveShopAsync(shop);
+        await Navigation.PopAsync();
+    }
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        var address = shop.Adress;
+        var locations = await Geocoding.GetLocationsAsync(address);
+
+        var options = new MapLaunchOptions
+        {
+            Name = "Magazinul meu preferat"
+        };
+        var shoplocation = locations?.FirstOrDefault();
+        /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
+        Windows Machine */
+
+        await Map.OpenAsync(shoplocation, options);
+
+    }
+}
