1af5875ac4c12e623e0bc8ef207d1bb6203f8381
ooirina <200990762+ooirina@users.noreply.github.com>
2025-12-13 00:48:44 +0200

    Lab10- a localiza magazine pe harta in functie de o adresa, utilizand google maps, pe telefon

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,4 +1,5 @@
 using OlaruIrinaLab7.Models;
+using Plugin.LocalNotification;
 
 namespace OlaruIrinaLab7;
 
@@ -25,8 +26,25 @@ public partial class ShopPage : ContentPage
             Name = "Magazinul meu preferat"
         };
         var shoplocation = locations?.FirstOrDefault();
+        var myLocation = await Geolocation.GetLocationAsync();
         /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
         Windows Machine */
+
+        var distance = myLocation.CalculateDistance(shoplocation,DistanceUnits.Kilometers);
+        if (distance < 5)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
 
         await Map.OpenAsync(shoplocation, options);
 
